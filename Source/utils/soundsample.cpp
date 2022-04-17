f8f469e530227a53f81763b7b80bc6fe8cdc1227
Chance4us <45902449+Chance4us@users.noreply.github.com>
2022-04-17 13:06:39 +0200

    Update soundsample.cpp

--- Source/utils/soundsample.cpp
+++ Source/utils/soundsample.cpp
@@ -6,6 +6,7 @@
 
 #include <Aulib/DecoderDrmp3.h>
 #include <Aulib/DecoderDrwav.h>
+#include <Aulib/ResamplerSpeex.h>
 #include <SDL.h>
 #ifdef USE_SDL1
 #include "utils/sdl2_to_1_2_backports.h"
@@ -15,7 +16,7 @@
 
 #include "engine/assets.hpp"
 #include "options.h"
-#include "utils/aulib.hpp"
+//#include "utils/aulib.hpp"
 #include "utils/log.hpp"
 #include "utils/math.h"
 #include "utils/stubs.h"
@@ -66,7 +67,7 @@ std::unique_ptr<Aulib::Decoder> CreateDecoder(bool isMp3)
 
 std::unique_ptr<Aulib::Stream> CreateStream(SDL_RWops *handle, bool isMp3)
 {
-	return std::make_unique<Aulib::Stream>(handle, CreateDecoder(isMp3), CreateAulibResampler(), /*closeRw=*/true);
+	return std::make_unique<Aulib::Stream>(handle, CreateDecoder(isMp3), std::make_unique<Aulib::ResamplerSpeex>(*sgOptions.Audio.resamplingQuality), /*closeRw=*/true);
 }
 
 /**
