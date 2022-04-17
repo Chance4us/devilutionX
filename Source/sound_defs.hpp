a34cd0cb360bd0b98f3f425b7a4b9f09632101ab
Chance4us <45902449+Chance4us@users.noreply.github.com>
2022-04-17 13:02:25 +0200

    Update sound_defs.hpp

--- Source/sound_defs.hpp
+++ Source/sound_defs.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <SDL_version.h>
+//#include <SDL_version.h>
 
 #define VOLUME_MIN -1600
 #define VOLUME_MAX 0
@@ -12,6 +12,6 @@
 #define PAN_MIN -6400
 #define PAN_MAX 6400
 
-#if SDL_VERSION_ATLEAST(2, 0, 7)
-#define DVL_AULIB_SUPPORTS_SDL_RESAMPLER
-#endif
+//#if SDL_VERSION_ATLEAST(2, 0, 7)
+//#define DVL_AULIB_SUPPORTS_SDL_RESAMPLER
+//#endif
