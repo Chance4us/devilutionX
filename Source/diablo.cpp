bc728bad205ad217318a6d8c2f879ca26b97abe2
Chance4us <45902449+Chance4us@users.noreply.github.com>
2023-02-03 00:55:30 +0100

    Update diablo.cpp

--- Source/diablo.cpp
+++ Source/diablo.cpp
@@ -1804,7 +1804,7 @@ void InitKeymapActions()
 	    "Screenshot",
 	    N_("Screenshot"),
 	    N_("Takes a screenshot."),
-	    SDLK_PRINTSCREEN,
+	    SDLK_F4,
 	    nullptr,
 	    CaptureScreen);
 	sgOptions.Keymapper.AddAction(
